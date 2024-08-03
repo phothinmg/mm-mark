9eec1e15a333933b0950513afae223f25029d149
Pho Thin <phothinmg@disroot.org>
2024-08-03 16:14:54 +0630

    modified:   jsr/tohtml.ts

--- jsr/tohtml.ts
+++ jsr/tohtml.ts
@@ -19,20 +19,20 @@ import { FrontMatter, type FrontMatterResult } from "./frontmatter.js";
  * @see https://github.com/showdownjs/showdown#options
  */
 export class ConvertMd<T> extends Showdown.Converter {
-  private md: FrontMatterResult<T>;
-  private yamldata: T;
-  private content: string;
+	private md: FrontMatterResult<T>;
+	private yamldata: T;
+	private content: string;
 
-  constructor(mdcontent: string, options?: MmConverter) {
-    super(getOptions(options));
-    this.md = new FrontMatter(mdcontent).frontmatter<T>();
-    this.yamldata = this.md.data;
-    this.content = this.md.content;
-  }
-  get data(): FrontMatterResult<T>["data"] {
-    return this.yamldata;
-  }
-  get html(): string {
-    return this.makeHtml(this.content);
-  }
+	constructor(mdcontent: string, options?: MmConverter) {
+		super(getOptions(options));
+		this.md = new FrontMatter(mdcontent).frontmatter<T>();
+		this.yamldata = this.md.data;
+		this.content = this.md.content;
+	}
+	get data(): FrontMatterResult<T>["data"] {
+		return this.yamldata;
+	}
+	get html(): string {
+		return this.makeHtml(this.content);
+	}
 }
